a7a3a54d3176162d26e4feb594b3bddcf7d36977
Alexander Lehmann <afwlehmann@gmail.com>
2008-10-22 17:02:10 +0000

    Added licensing info.

--- PlaybackThread.cpp
+++ PlaybackThread.cpp
@@ -1,5 +1,22 @@
 //
+// PlaybackThread.cpp
 // $Id$
+//
+// Copyright 2008 by Alexander Lehmann <lehmanna@in.tum.de>
+//
+// This file is part of annot8.
+// 
+// annot8 is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+// 
+// annot8 is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with
+// annot8.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 
